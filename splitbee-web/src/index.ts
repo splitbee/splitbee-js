06f57799ba22ed0ff968c567015da3663f61f1ca
Tobias Lins <me@tobi.sh>
2021-01-18 20:42:16 +0100

    Fix typings and add support for attributes

--- splitbee-web/src/index.ts
+++ splitbee-web/src/index.ts
@@ -1,42 +1,55 @@
-type QueueData = {
-  type: 'user' | 'event';
-  payload: any;
+import { Splitbee } from './types';
+
+const SCRIPT_URL = 'https://cdn.splitbee.io/sb.js';
+
+type SplitbeeOptions = {
+  disableCookie?: boolean;
+  token?: string;
+  api?: string;
 };
 
-const start = (
-  win: Window & typeof globalThis,
-  doc: Document,
-  name: any,
-  src: string
-) => {
-  if (window.splitbee) return;
+export const init = (options?: SplitbeeOptions) => {
+  if (typeof window === 'undefined' || window.splitbee) return;
   const queue: Array<QueueData> = [];
-  function addToQueue(type: QueueData['type']) {
-    return function() {
-      queue.push({ type: type, payload: arguments });
+  const createAddToQueue = (type: QueueData['type']) => {
+    return (...args: any) => {
+      queue.push({ type: type, payload: args });
     };
-  }
-  win[name] = win[name] || {
-    track: addToQueue('event'),
-    user: {
-      set: addToQueue('user'),
-    },
   };
 
-  const script = doc.createElement('script');
-  script.src = src;
+  window.splitbee =
+    window.splitbee ||
+    ({
+      track: createAddToQueue('event'),
+      user: {
+        set: createAddToQueue('user'),
+      },
+    } as Splitbee);
+
+  const script = document.createElement('script');
+  script.src = SCRIPT_URL;
   script.async = true;
+
+  if (options) {
+    if (options.api) script.dataset.api = options.api;
+    if (options.token) script.dataset.token = options.token;
+    if (options.disableCookie) script.dataset.noCookie = '1';
+  }
+
   script.onload = function() {
     queue.forEach(ev => {
       if (ev.type === 'event') window.splitbee.track.apply(null, ev.payload);
       if (ev.type === 'user') window.splitbee.user.set.apply(null, ev.payload);
     });
   };
-  doc.head.appendChild(script);
+  document.head.appendChild(script);
 };
-
-start(window, document, 'splitbee', 'https://cdn.splitbee.io/sb.js');
 
 export default () => {
   return window.splitbee;
+};
+
+type QueueData = {
+  type: 'user' | 'event';
+  payload: any;
 };
--- splitbee-web/src/types.d.ts
+++ splitbee-web/src/types.d.ts
@@ -1,8 +1,16 @@
 export {};
 
+type Data = { [key: string]: string | number | boolean };
+
+export type Splitbee = {
+  track: (event: string, data?: Data) => Promise<void>;
+  user: {
+    set: (data: Data) => Promise<void>;
+  };
+};
+
 declare global {
   interface Window {
-    // your types here!
-    splitbee: any;
+    splitbee: Splitbee;
   }
 }
